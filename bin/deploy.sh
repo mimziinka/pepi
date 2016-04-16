28f6ad127610238a104358bee36ec663b17c01c9
Blagovest Dachev <blago@dachev.com>
2016-04-16 19:10:14 +0700

    Created a deployment script

---
+++ bin/deploy.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rsync -vza --delete-excluded --exclude .git /Users/blago/Projects/pepi/www.peterslavov.com/src/ u64720708@s377785186.onlinehome.us:/kunden/homepages/16/d377785170/htdocs/pepi
